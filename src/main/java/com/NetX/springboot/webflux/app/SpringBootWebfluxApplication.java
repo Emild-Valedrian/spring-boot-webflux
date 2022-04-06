ec47d9c84ebff650adf17e75be627f41f590f471
Emild-Valedrian <88640226+Emild-Valedrian@users.noreply.github.com>
2022-04-06 00:03:05 -0500

    last_git

--- src/main/java/com/NetX/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/NetX/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -10,43 +10,54 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
-import com.NetX.springboot.webflux.app.models.dao.ProductoDao;
+import com.NetX.springboot.webflux.app.models.documents.Categoria;
 import com.NetX.springboot.webflux.app.models.documents.Producto;
+import com.NetX.springboot.webflux.app.models.services.ProductoService;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication implements CommandLineRunner{
+public class SpringBootWebfluxApplication implements CommandLineRunner {
 
 	@Autowired
-	private ProductoDao dao; 
-	
+	private ProductoService service;
+
 	@Autowired
-	private ReactiveMongoTemplate mongoTemplate; 
-	
-	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class); 
-	
+	private ReactiveMongoTemplate mongoTemplate;
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		mongoTemplate.dropCollection("productos").subscribe();
-		
-		Flux.just(new Producto("Sega Saturn", 399.00),
-				new Producto("PlayStation", 299.00),
-				new Producto("Nintendo 64", 299.00),
-				new Producto("3DO", 380.00),
-				new Producto("Atari Jaguar", 280.00),
-				new Producto("Apple PipPin", 400.00)
-				)
-		.flatMap(producto -> {
-			producto.setCreateAt(new Date());
-			return dao.save(producto);
-			})
-		.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));		
+		mongoTemplate.dropCollection("categorias").subscribe();
+
+		Categoria nintendo = new Categoria("Nintendo");
+		Categoria sega = new Categoria("Sega");
+		Categoria atari = new Categoria("Atari");
+		Categoria sony = new Categoria("Sony");
+		Categoria apple = new Categoria("Apple");
+		Categoria panasonic = new Categoria("Panasonic");
+
+		Flux.just(nintendo, sega, atari, sony, apple, panasonic).flatMap(service::saveCategoria).doOnNext(c -> {
+			log.info("Categoria creada: " + c.getNombre() + "/ Id: " + c.getId());
+		}).thenMany(Flux
+				.just(new Producto("Sega Saturn", 399.00, sega), 
+					  new Producto("PlayStation", 299.00, sony),
+					  new Producto("Nintendo 64", 299.00, nintendo), 
+					  new Producto("3DO", 380.00, panasonic),
+					  new Producto("Atari Jaguar", 280.00, atari), 
+					  new Producto("Apple PipPin", 400.00, apple))
+				.flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return service.save(producto);
+				}))
+				.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 	}
 
 }
--- src/main/java/com/NetX/springboot/webflux/app/models/documents/Producto.java
+++ src/main/java/com/NetX/springboot/webflux/app/models/documents/Producto.java
@@ -2,23 +2,44 @@ package com.NetX.springboot.webflux.app.models.documents;
 
 import java.util.Date;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Document(collection = "productos")
 public class Producto {
 	
 	@Id
 	private String id; 	
-	private String nombre; 
+	
+	@NotEmpty
+	private String nombre;
+	
+	@NotNull
 	private Double precio; 
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 			
+	@Valid
+	private Categoria categoria; 
+	
+	private String foto; 
+	
 	public Producto() {}
 
 	public Producto(String nombre, Double precio) {		
 		this.nombre = nombre;
 		this.precio = precio;
+	}
+	
+	public Producto(String nombre, Double precio, Categoria categoria) {		
+		this(nombre, precio);
+		this.categoria= categoria; 
 	}
 	
 	public String getId() {
@@ -45,6 +66,21 @@ public class Producto {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}	
 	
 }
